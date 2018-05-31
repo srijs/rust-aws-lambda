0bcc1ac95e62420d479174e10bc71c61d34ff49d
Sam Rijs <srijs@airpost.net>
2018-05-31 20:20:36 +1000

    docs(lib): add comment to ::start function

--- aws_lambda_runtime/src/lib.rs
+++ aws_lambda_runtime/src/lib.rs
@@ -26,6 +26,27 @@ mod server;
 pub use context::Context;
 pub use runtime::Runtime;
 
+/// Start the lambda runtime using the provided handler function.
+///
+/// The function will block until the runtime shuts down or returns
+/// with an error.
+///
+/// ## Example
+///
+/// ```no_run
+/// extern crate aws_lambda_runtime as lambda;
+///
+/// fn main() {
+///     lambda::start(|()| Ok("Hello ƛ!"))
+/// }
+/// ```
+///
+/// ## Panics
+///
+/// This function will panic if it fails to create the runtime.
+///
+/// If you wish to handle this case more, you can use `Runtime::start`
+/// instead.
 pub fn start<F, R, S>(f: F)
 where
     F: Fn(R) -> S + 'static,
