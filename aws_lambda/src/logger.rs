0885ce73b05db45d2e1af3901b8148fbaafa1820
Sam Rijs <srijs@airpost.net>
2018-07-12 21:35:08 +1000

    fix(logger): use static variable to avoid boxing

--- aws_lambda/src/logger.rs
+++ aws_lambda/src/logger.rs
@@ -2,9 +2,11 @@ use log::{self, Level, LevelFilter, Log, Metadata, Record};
 
 /// Initialize the logging system.
 pub fn init() {
-    log::set_boxed_logger(Box::new(Logger)).unwrap();
+    log::set_logger(&LOGGER).unwrap();
     log::set_max_level(LevelFilter::Info);
 }
+
+static LOGGER: Logger = Logger;
 
 struct Logger;
 
