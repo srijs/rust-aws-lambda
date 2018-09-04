f310bce2dbcdea423d9a9d5e4ca25c4248c7aed9
Sam Rijs <srijs@airpost.net>
2018-09-05 00:53:29 +1000

    test(*): fix skeptic build script

--- aws_lambda/build.rs
+++ aws_lambda/build.rs
@@ -4,7 +4,7 @@ use skeptic::*;
 
 fn main() {
     let mut files = markdown_files_of_directory("../docs/");
-    files.push("../README.md");
+    files.push("../README.md".into());
 
     generate_doc_tests(&files);
 }
